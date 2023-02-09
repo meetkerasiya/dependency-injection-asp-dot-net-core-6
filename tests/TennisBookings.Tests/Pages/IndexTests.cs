8c28c5f0bebd06ecef0aa48f18d5ec31fbd0fb55
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-09 12:00:01 +0530

    Testing using mock classes added

--- tests/TennisBookings.Tests/Pages/IndexTests.cs
+++ tests/TennisBookings.Tests/Pages/IndexTests.cs
@@ -5,7 +5,7 @@ public class IndexTests
 	[Fact]
 	public async Task ReturnsExpectedViewModel_WhenWeatherIsSun()
 	{
-		var sut = new IndexModel();
+		var sut = new IndexModel(new SunForecaster(),NullLogger<IndexModel>.Instance);
 
 		await sut.OnGet();
 
@@ -15,12 +15,39 @@ public class IndexTests
 	[Fact]
 	public async Task ReturnsExpectedViewModel_WhenWeatherIsRain()
 	{
-		var sut = new IndexModel();
+		var sut = new IndexModel(new RainForecaster(), NullLogger<IndexModel>.Instance);
 
 		await sut.OnGet();
 
 		Assert.Contains("We're sorry but it's raining here.", sut.WeatherDescription);
 	}
 
-
+	private class SunForecaster : IWeatherForecaster
+	{
+		public Task<WeatherResult> GetCurrentWeatherAsync(string city)
+		{
+			return Task.FromResult(new WeatherResult
+			{
+				City = city,
+				Weather = new WeatherCondition
+				{
+					Summary = "Sun"
+				}
+			});
+		}
+	}
+	private class RainForecaster : IWeatherForecaster
+	{
+		public Task<WeatherResult> GetCurrentWeatherAsync(string city)
+		{
+			return Task.FromResult(new WeatherResult
+			{
+				City = city,
+				Weather = new WeatherCondition
+				{
+					Summary = "Rain"
+				}
+			});
+		}
+	}
 }
